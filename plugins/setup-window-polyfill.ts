5696003ef91e8339c464e36879eaf993935103b3
lijing <flm894634186@qq.com>
2026-03-28 17:03:14 +0800

    清空非.md 文件，仅保留文档

--- plugins/setup-window-polyfill.ts
+++
@@ -1,8 +0,0 @@
-// 为 SSR 环境提供 window polyfill
-export default defineNuxtPlugin(() => {
-  // 只在服务端执行
-  if (import.meta.server) {
-    // @ts-ignore - 为 globalThis 添加 window 别名
-    global.window = globalThis
-  }
-})
